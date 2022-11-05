00318dc0976fdbc28792b0aea9f562ce84aa47db
applebuddy <eagermin92@gmail.com>
2022-11-06 03:00:24 +0900

    Making PinchApp, 28. 1. Setting up the new iOS iPhone and iPad project

---
+++ Pinch/Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by MinKyeongTae on 2022/11/06.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
---
+++ Pinch/Pinch/Screen/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  Pinch
+//
+//  Created by MinKyeongTae on 2022/11/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  // MARK: - Property
+  
+  // MARK: - Function
+  
+  // MARK: - Content
+
+  var body: some View {
+    Text("Hello, world!")
+      .padding()
+  }
+}
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView()
+      .preferredColorScheme(.dark)
+  }
+}
