7a3300dbb2b05e3bfa0b0b1991421fbfec21872d
applebuddy <eagermin92@gmail.com>
2022-11-12 00:21:55 +0900

    Making FruitsApp, 38. 1. Let's set up the iOS project in Xcode
    making app preparation

---
+++ Fruits/Fruits/FruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsApp.swift
+//  Fruits
+//
+//  Created by MinKyeongTae on 2022/11/12.
+//
+
+import SwiftUI
+
+@main
+struct FruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
