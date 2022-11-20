51ac2cd99824ce19521d86516b7eab85092c499b
applebuddy <eagermin92@gmail.com>
2022-11-20 20:03:51 +0900

    Making AfricaApp, 53. 4. Let's develop the detaili view for each animal with dynamic data - Part 1
    learn about
    #horizontal scrollView with ForEach, navigationLink(iOS13+),

---
+++ Africa/Africa/View/AnimalDetailView.swift
@@ -0,0 +1,75 @@
+//
+//  AnimalDetailView.swift
+//  Africa
+//
+//  Created by MinKyeongTae on 2022/11/20.
+//
+
+import SwiftUI
+
+struct AnimalDetailView: View {
+  // MARK: - Properties
+  
+  let animal: Animal
+  
+  // MARK: - Body
+  
+  var body: some View {
+    ScrollView(.vertical, showsIndicators: false) {
+      VStack(alignment: .center, spacing: 20) {
+        // Hero Image
+        Image(animal.image)
+          .resizable()
+          .scaledToFit()
+        
+        // Title
+        Text(animal.name.uppercased())
+          .font(.largeTitle)
+          .fontWeight(.heavy)
+          .multilineTextAlignment(.center)
+          .padding(.vertical, 8)
+          .foregroundColor(.primary)
+          .background(
+            Color.accentColor
+              .frame(height: 6)
+              .offset(y: 24)
+          )
+        
+        // Headline
+        Text(animal.headline)
+          .font(.headline)
+          .multilineTextAlignment(.leading)
+          .foregroundColor(.accentColor)
+          .padding(.horizontal)
+        
+        // Gallery
+        Group {
+          HeadingView(headingImage: "photo.on.rectangle.angled", headingText: "Wilderness in Pictures")
+          
+          InsetGalleryView(animal: animal)
+        }
+        .padding(.horizontal)
+        
+        // Facts
+        
+        // Description
+        
+        // Map
+        
+        // Link
+
+      } //: VStack
+      .navigationBarTitle("Learn abount \(animal.name)", displayMode: .inline)
+    } //: Scroll
+  }
+}
+
+struct AnimalDetailView_Previews: PreviewProvider {
+  static let animals: [Animal] = Bundle.main.decode("animals.json")
+
+  static var previews: some View {
+    NavigationView {
+      AnimalDetailView(animal: animals[0])
+    }
+  }
+}
